5b17d61c938754fd277b3a73ac724a672bceacc4
JIanzhu Huai <huai.3@osu.edu>
2021-10-20 13:00:15 +0800

    build on ubuntu 18

--- build.sh
+++ build.sh
@@ -1,5 +1,21 @@
 echo "Configuring and building Thirdparty/DBoW2 ..."
 
+ORBSLAM_DIR=$(pwd)
+Pangolin_DIR=$ORBSLAM_DIR/Thirdparty/Pangolin/devel/lib/cmake/Pangolin # Directory containing PangolinConfig.cmake.
+
+buildAndInstallPangolin() {
+cd Thirdparty
+# git clone https://github.com/stevenlovegrove/Pangolin
+cd Pangolin && git checkout v0.6
+mkdir -p devel
+mkdir -p build && cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-std=c++11 -DCMAKE_INSTALL_PREFIX=$ORBSLAM_DIR/Thirdparty/Pangolin/devel ..
+make -j4 && make install
+cd ../../../
+}
+
+buildAndInstallPangolin
+
 cd Thirdparty/DBoW2
 mkdir build
 cd build
@@ -27,5 +43,5 @@ echo "Configuring and building ORB_SLAM3 ..."
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DPangolin_DIR=$Pangolin_DIR
 make -j
